1e68427aa1c80c85245bdaabd01122142df521b4
Lorenze Jay <Lorenzejay@gmail.com>
2021-05-18 06:01:17 -0700

    adjusted sizing

--- components/remembering.jsx
+++ components/remembering.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Remembering = () => {
   return (
     <div
-      className="flex items-center justify-center flex-col pt-40 h-screen px-5 md:px-7 lg:px-16 xl:px-20 2xl:px-24"
+      className="flex items-center justify-center flex-col pt-40 pb-20 h-auto lg:h-screen px-5 md:px-7 lg:px-16 xl:px-20 2xl:px-24"
       style={{ background: "#dceae6" }}
     >
       <main style={{ color: "#C79288" }}>
@@ -18,11 +18,11 @@ const Remembering = () => {
       <div className="flex flex-col lg:flex-row items-center justify-around mt-3">
         <img
           src="/mahjongtiles.png"
-          className="w-64 h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 object-cover m-3"
+          className="w-64 h-64 md:w-72 md:h-72 2xl:w-96 2xl:h-96 object-cover m-3"
         />
         <img
           src="/lola2.jpg"
-          className="w-64 h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 object-cover rounded-full object-fit object-right m-3"
+          className="w-64 h-64 md:w-72 md:h-72 2xl:w-96 2xl:h-96 object-cover rounded-full object-fit object-right m-3"
         />
       </div>
     </div>
